acb11f8f9b9af70d10eb68b5785d10ad71ec3cdb
ivz-dev <zakutynsky@yahoo.com>
2017-11-07 11:31:58 +0200

    fixed delete message

--- src/routes/deleteTunnel.php
+++ src/routes/deleteTunnel.php
@@ -38,7 +38,7 @@ $app->post('/api/SauceLabs/deleteTunnel', function ($request, $response) {
             $result['callback'] = 'success';
             $result['contextWrites']['to'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
             if(empty($result['contextWrites']['to'])) {
-                $result['contextWrites']['to']['status_msg'] = "Api return no results";
+                $result['contextWrites']['to']['status_msg'] = "Tunnel successfully deleted!";
             }
         } else {
             $result['callback'] = 'error';
